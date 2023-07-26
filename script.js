745a0d28ccd37437c7d7627b7cac7fc34904848a
tarikjamil <tarikjamill@gmail.com>
2023-07-26 22:49:59 +0100

    edits

--- script.js
+++ script.js
@@ -237,6 +237,28 @@ $(".navbar--menu-trigger").click(function () {
   $(this).data("clicks", !clicks);
 });
 
-$(".menu-open-bg").on("click", function () {
-  $(".navbar--menu-trigger").click();
+// accordion navbar --------------------- //
+$(".navbar-link.is-dropdown").on("click", function () {
+  // Close other accordions when opening new one
+  if (!$(this).hasClass("open")) {
+    $(".navbar-dropdown-list.open").click();
+  }
+  // Save the sibling of the toggle we clicked on
+  let sibling = $(this).siblings(".navbar-dropdown-list");
+  let animationDuration = 500;
+
+  if ($(this).hasClass("open")) {
+    // Close the content div if already open
+    sibling.animate({ height: "0px" }, animationDuration);
+  } else {
+    // Open the content div if already closed
+    sibling.css("height", "auto");
+    let autoHeight = sibling.height();
+    sibling.css("height", "0px");
+    sibling.animate({ height: autoHeight }, animationDuration, () => {
+      sibling.css("height", "auto");
+    });
+  }
+  // Open and close the toggle div
+  $(this).toggleClass("open");
 });
